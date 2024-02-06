908490f6b402cc34190e6fe9ae852cd1c137936c
zasked98 <159190106+zasked98@users.noreply.github.com>
2024-02-06 17:40:35 -0300

    Create list.rb
    archivo que contiene la logica del inicio de la lista de tareas del programa

---
+++ list.rb
@@ -0,0 +1,37 @@
+require "./item.rb"
+
+class List
+    attr_writer :items
+
+    def initialize()
+        @items = Array.new()
+    end
+
+    def add_item(text)
+        item = Item.new(text)
+        @items.push(item)
+    end
+
+    def remove_item(index)
+        @items.delete_at(index)
+    end
+    
+    def check_item(index)
+        @items[index].checked = true
+    end
+    
+    def remove_all
+        @items.clear
+    end
+
+    def show_all
+        if @items.length == 0
+            puts "no hay tareas en tu lista"
+        else
+            @items.each_index do |index|
+                puts index.to_s + " - " + @items[index].to_s
+            end
+        end
+    end
+         
+end
